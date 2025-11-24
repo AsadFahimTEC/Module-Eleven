d1d0cec7a6ea614480b15aeec245c3b4366be9d9
AsadFahimTEC <fah485434@gmail.com>
2025-11-24 11:50:15 +0600

    one to eight class added

---
+++ src/routes/index.ts
@@ -0,0 +1,36 @@
+import { readUsers, writeUsers } from "../helpers/fileDB";
+import parseBody from "../helpers/parseBody";
+import addRoutes from "../helpers/RouteHandler";
+import sendJson from "../helpers/sendJson";
+
+addRoutes("GET", "/", (req, res) => {
+    sendJson(res, 200, {
+        message: "Hello from node js with typescript...",
+        path: req.url,
+    });
+});
+
+addRoutes("GET", "/api", (req, res) => {
+    sendJson(res, 200, {
+        message: "Health status ok",
+        path: req.url,
+    });
+});
+
+addRoutes("POST", "/api/users", async (req, res) => {
+    const body = await parseBody(req);
+
+    // user json read
+    const users = readUsers();
+
+    const newUser = {
+        id: Date.now(), // create unique id
+        ...body,
+    };
+
+    users.push(newUser);
+
+    writeUsers(users);
+
+    sendJson(res, 201, {success: true, data: body});
+})
---
+++ src/server.ts
@@ -0,0 +1,34 @@
+import http, { IncomingMessage, Server, ServerResponse } from "http";
+import config from "./config";
+import addRoutes, { RouteHandler, routes } from "./helpers/RouteHandler";
+import "./routes";
+
+
+const server: Server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
+
+    const method = req.method?.toUpperCase() || "";
+    const path = req.url || "";
+
+    const methodMap = routes.get(method);
+    const handler: RouteHandler | undefined = methodMap?.get(path);
+
+    if (handler) {
+        handler(req, res);
+    } else {
+        res.writeHead(404, { "content-type": "application/json" });
+        res.end(JSON.stringify({
+            success: false,
+            message: "Route not found!!!",
+            path,
+        })
+        )
+    }
+
+}
+
+);
+
+server.listen(config.port, () => {
+    console.log(`server is running on port ${config.port}`);
+});
+
