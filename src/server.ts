eb22bdba1e41847367b71058ea22c82f97bdf302
AsadFahimTEC <fah485434@gmail.com>
2025-11-24 19:15:15 +0600

    class 10 added

--- src/server.ts
+++ src/server.ts
@@ -2,40 +2,9 @@ import http, { IncomingMessage, Server, ServerResponse } from "http";
 import config from "./config";
 import addRoutes, { RouteHandler, routes } from "./helpers/RouteHandler";
 import "./routes";
+import findDynamicRoute from "./helpers/dynamicRouterHandler";
 
-function findDynamicRoute(method: string, url: string) {
-    const methodMap = routes.get(method);
-    if (!methodMap) return null;
 
-    for (const [routePath, handler] of methodMap.entries()) {
-
-        // "/api/users/:id" --> ["api", "users", ":id"]
-        const routeParts = routePath.split("/");
-        // "/api/users/1" --> ["api", "users", "10"]
-        const urlParts = url.split("/")
-
-        if (routeParts.length !== urlParts.length) continue;
-
-        const params: any = {}; //{id: 2}
-        let matched = true
-
-        // "api/users/:id"
-        for (let i = 0; i < routeParts.length; i++) {
-            if (routeParts[i]?.startsWith(":")) {
-                params[routeParts[i]?.substring(1)!] = urlParts[i]; // {id:1}
-            } else if (routeParts[i] !== urlParts[i]) {
-                matched = false;
-                break;
-            }
-        }
-
-        if (matched) {
-            return { handler, params };
-        }
-    }
-
-    return null;
-}
 const server: Server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
 
     const method = req.method?.toUpperCase() || "";
